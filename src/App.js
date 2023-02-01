b504087d833857554ebe6c495b3b57d4c3a622d9
rajasekhar518 <115791622+rajasekhar518@users.noreply.github.com>
2023-02-01 17:20:06 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
+import MyCard from './components/MyCard';
+import TourCards from './components/tour-cards/TourCards';
+import NavBar from './layout/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <NavBar heading="Hello"/>
+     {/* <MyCard heading="Good Morning" />
+     <MyCard heading="Good Afternoon" />
+     <MyCard heading="Good Evening" /> */}
+     <TourCards />
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+/* bootstrap import*/
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import "../node_modules/bootstrap-icons/font/bootstrap-icons.css";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
